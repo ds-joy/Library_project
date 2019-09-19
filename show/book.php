672dfd6d7aab700e60fe4156f597745891cd490e
ds-joy <debasishsarker36@gmail.com>
2019-09-20 01:19:38 +0600

    show book

--- show/book.php
+++ show/book.php
@@ -1,11 +1,7 @@
 <?php
 session_start();
 $con=mysqli_connect('localhost','root','');
-<<<<<<< HEAD
 mysqli_select_db($con,'library');
-=======
-mysqli_select_db($con,'rlibrary');
->>>>>>> e31fabb2c246fc563cb9d2a83dec5814944b4a93
 $s= "select * from book" ;
 $result=mysqli_query($con,$s);
 ?>
@@ -15,7 +11,6 @@ $result=mysqli_query($con,$s);
         <title>
         Book Data
         </title>
-<<<<<<< HEAD
         <link rel="stylesheet" href="../css/bootstrap.min.css">
     </head>
     
@@ -26,38 +21,6 @@ $result=mysqli_query($con,$s);
         <table class="table table-striped">
             <tr>
                 <th colspan="3"><h1 class="text-center">Book Table</h1></th>
-=======
-    </head>
-    <style>
-        body{
-            background:Seashell;
-        }
-        table{
-            width:800px;
-            margin:auto;
-            text-align:center;
-            table-layout:fixed;
-        }
-        table,tr,th,td{
-            padding:20px;
-            color:white;
-            border:1px solid #080808;
-            border-collapse:collapse;
-            font-size:18px;
-            font-family:Arial;
-            background:linear-gradient(top,#3c3c3c 0%,#222222 100%);
-            background:-webkit-linear-gradient(top,#3c3c3c 0%,#222222 100%);
-        }
-        
-        
-        
-    </style>
-    <body>
-        
-        <table>
-            <tr>
-                <th colspan="3"><h1>Book Table</h1></th>
->>>>>>> e31fabb2c246fc563cb9d2a83dec5814944b4a93
             <tr>
             <tr>
                 <th> Book No</th>
@@ -84,10 +47,7 @@ $result=mysqli_query($con,$s);
             ?>
             
         </table>
-<<<<<<< HEAD
         <script type="text/javascript" src="../js/bootstrap.min.js"></script>
         </div>
-=======
->>>>>>> e31fabb2c246fc563cb9d2a83dec5814944b4a93
     </body>
 </html>
